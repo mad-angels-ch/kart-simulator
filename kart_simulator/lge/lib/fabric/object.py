0197f05e6daf56edc810f0d20e65ded54d09f6bc
LJ44 <admin@lj44.ch>
2021-10-01 23:11:45 +0200

    suite de fabric, pas fonctionnel

---
+++ kart_simulator/lge/lib/fabric/object.py
@@ -0,0 +1,12 @@
+import core.objects as objects
+
+from lib.point import Point
+
+
+def createObject(objectData) -> objects.Object:
+    # if objectData["type"] == "group":
+    #     map(createObject, objectData["objects"])
+    #     return ???
+
+    # else:
+    pass
