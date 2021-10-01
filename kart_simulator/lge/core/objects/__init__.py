3afaa42873731f3be2694b71a800bb4fc5a3cc56
LJ44 <admin@lj44.ch>
2021-10-01 23:12:10 +0200

    ajout de la "fonction" objects.create

--- kart_simulator/lge/core/objects/__init__.py
+++ kart_simulator/lge/core/objects/__init__.py
@@ -1,5 +1,5 @@
 from core.objects.object import Object
 from core.objects.circle import Circle
 
-from core.objects.factory import Factory
+from core.objects.factory import Factory, create
 from core.objects.container import Container
