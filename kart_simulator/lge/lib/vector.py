bdbb63711b3e2b580f5664984193bc79a520b5de
LJ44 <admin@lj44.ch>
2021-10-07 16:26:37 +0200

    amélioration (TRESSS lourde) de la classe vector

--- kart_simulator/lge/lib/vector.py
+++ kart_simulator/lge/lib/vector.py
@@ -1,27 +1,118 @@
+import math
+from typing import overload
+
 class Vector:
     _x: float
     _y: float
 
-    def __init__(self, x: float = 0, y: float = 0) -> None:
+    @overload
+    def __init__(self, x: float = 0, y: float = 0):
         self._x = x
         self._y = y
 
-    def x(self) -> float:
-        return self._x
+    @overload
+    def __init__(self, vector: "Vector"):
+        self._x = vector._x
+        self._y = vector._y
 
-    def y(self) -> float:
-        return self._y
+    def __len__(self) -> int:
+        return 2
 
-    def get_x(self) -> float:
-        return self._x
+    def __iter__(self):
+        return (self._x, self._y)
 
-    def get_y(self) -> float:
-        return self._y
+    def __neg__(self) -> "Vector":
+        return Vector(-self._x, self._y)
 
-    def scale(self, factor: float) -> None:
-        "Multiplie le vecteur par le facteur donné"
-        self._x *= factor
-        self._y *= factor
+    def __pos__(self) -> "Vector":
+        return self
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self._x + other._x, self._y + other._y)
+
+    def __iadd__(self, other: "Vector") -> None:
+        for i in range(len(self)):
+            self[i] = self[i] + other[i]
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return self + (-other)
+
+    def __isub__(self, other: "Vector") -> None:
+        for i in range(len(self)):
+            self[i] = self[i] - other[i]
+
+    @overload
+    def __mul__(self, other: float) -> "Vector":
+        return Vector(self._x * other, self._y * other)
+
+    def __imul__(self, other: float) -> None:
+        for i in range(len(self)):
+            self[i] = self[i] * other[i]
+
+    @overload
+    def __mul__(self, other: "Vector") -> float:
+        "Retourne le produit scalaire des deux vecteurs"
+        return self._x * other._x + self._y * other._y
+
+    def __truediv__(self, other: float) -> "Vector":
+        return Vector(self._x / other, self._y / other)
+
+    def __idiv__(self, other: float) -> None:
+        for i in range(len(self)):
+            self[i] = self[i] / other[i]
+
+    def __pow__(self, other: int) -> float:
+        if other != 2:
+            raise ValueError()
+        return self * self
+
+    def __eq__(self, o: "Vector") -> bool:
+        return self._x == o._x and self._y == o._y
+
+    def __ne__(self, o: object) -> bool:
+        return not self == o
+
+    @overload
+    def __getitem__(self, index: int) -> float:
+        if index == 0:
+            return self._x
+        elif index == 1:
+            return self._y
+        else:
+            raise ValueError()
+
+    @overload
+    def __getitem__(self, index: str) -> float:
+        if index.lower() == "x":
+            return self._x
+        elif index.lower() == "y":
+            return self._y
+        else:
+            raise ValueError()
+
+    def norm(self) -> None:
+        return math.hypot(self._x, self._y)
+
+    def set_norm(self, newNorm: float) -> None:
+        self._x, self._y *= newNorm / self.norm()
+
+    def normalVector(self) -> "Vector":
+        return Vector(-self._y, self._x)
+
+    def direction(self) -> float:
+        """Retourne l'angle formé par ce vecteur et un vecteur de composantes 1 et 0.
+        L'angle est situé entre 0 et 2 Pi"""
+        return math.atan2(self._y, self._x)
+
+    def rotate(self, angle: float) -> None:
+        cos = math.cos(angle)
+        sin = math.sin(angle)
+        self._x = self._x * cos - self._y * sin
+        self._y = self._x * sin + self._y * cos
+
+    def orthogonalProjection(self, vector: "Vector") -> "Vector":
+        "Projete ce vecteur sur le vecteur donné en paramètre"
+        return self * (self * vector) / (vector._x ** 2)
 
     def scaleX(self, factor: float) -> None:
         "Multiplie la composante du vecteur par le facteur donné"
@@ -31,20 +122,18 @@ class Vector:
         "Multiplie la composante du vecteur par le facteur donné"
         self._y *= factor
 
-    def __iter__(self):
-        return VectorIterator(self)
+    def x(self) -> float:
+        "Obsolète, utiliser self[0] ou self['x']"
+        return self._x
 
+    def y(self) -> float:
+        "Obsolète, utiliser self[1] ou self['y']"
+        return self._y
 
-class VectorIterator:
-    def __init__(self, vector: Vector) -> None:
-        self._vector = vector
-        self._index = 0
+    def get_x(self) -> float:
+        "Obsolète, utiliser self[0] ou self['x']"
+        return self._x
 
-    def __next__(self) -> float:
-        if self._index == 0:
-            self._index += 1
-            return self._vector.get_x()
-        elif self._index == 1:
-            self._index += 1
-            return self._vector.get_y()
-        raise StopIteration
+    def get_y(self) -> float:
+        "Obsolète, utiliser self[1] ou self['y']"
+        return self._y
