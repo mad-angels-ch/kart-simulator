0b82cb82ec346d55c5949d25194aa774cef6da6e
Lorin Jacot <lorinjacot@gmail.com>
2021-10-20 11:35:01 +0200

    détection basique de collision entre 2 cercles

--- game/objects/Circle.py
+++ game/objects/Circle.py
@@ -6,3 +6,9 @@ class Circle(Object):
 
     def radius(self) -> float:
         return self._radius
+
+    def collides(self, object: "Object") -> bool:
+        if isinstance(object, Circle):
+            return self.center().distanceOf(object.center()) < self.radius() + object.radius()
+        else:
+            return object.collides(self)
