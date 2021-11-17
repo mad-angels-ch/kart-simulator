0308003d8898897c6107a1251fc3ede4cc28e0d1
Noé Thiran <noe.thiran@gmail.com>
2021-11-17 18:20:46 +0100

    Config de la page et destruction de la partie

--- kart_simulator_launcher.py
+++ kart_simulator_launcher.py
@@ -21,33 +21,38 @@ class MenuApp(App):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
+        self.game_instance = None
     def build(self):
         Window.clearcolor = get_color_from_hex("#ffffff")
         self.manager = MyScreenManager()
         return self.manager
-    
+        # return MainWidget()
 
     def start_ks(self, world):
         self.world = world
         if self.manager.has_screen("Kart_Simulator"):
             screen = self.manager.get_screen("Kart_Simulator")
             self.manager.remove_widget(screen)
-        game_instance = KS_screen(world)
-        self.manager.add_widget(game_instance)
+        self.game_instance = KS_screen(world)
+        self.manager.add_widget(self.game_instance)
         self.manager.push("Kart_Simulator")
         
+    def clear_game(self):
+        if self.game_instance:
+            self.game_instance.quit()
+            self.game_instance = None
+        
 
 
         
         
 
+from kivy.config import Config
+from kivy.core.window import Window
+# Window.fullscreen = 'auto'
+Config.set('kivy', 'exit_on_escape', '0')
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
-# from kivy.core.window import Window
-# Window.fullscreen = True
-
-# from kivy.config import Config
-# Config.set('graphics', 'fullscreen', 1)
 
 MenuApp().run()
 
