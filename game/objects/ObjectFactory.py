2f77a523607562aa7f9add1110cb666696310a63
LJ44 <admin@lj44.ch>
2022-01-09 14:56:36 +0100

    mise à jour du check des circutes + message en anglais

--- game/objects/ObjectFactory.py
+++ game/objects/ObjectFactory.py
@@ -128,7 +128,7 @@ class ObjectFactory:
 
         if gatesCount < 2:
             raise ObjectCountError("Gate", 2, gatesCount)
-        elif finishLineCount == 1:
+        elif finishLineCount != 1:
             raise ObjectCountError("Finish line", 1, finishLineCount)
         elif kartPlaceHolderCount < 1:
             raise ObjectCountError("Kart placeholder", 1, kartPlaceHolderCount)
@@ -147,7 +147,7 @@ class ObjectCountError(RuntimeError):
         self._foundCount = foundCount
 
     def message(self) -> str:
-        return f"Nous attendions {self._requiredCount} {self._type} mais trouvé {self._foundCount}"
+        return f"{self._requiredCount} {self._type} were expected but {self._foundCount} were found"
 
 
 create = ObjectFactory()
