87f0156f7e612639efa4ad3836f84f87054585c5
LJ44 <admin@lj44.ch>
2022-01-12 18:30:28 +0100

    ajout des commentaires à game.objects.fill

--- game/objects/fill/Fill.py
+++ game/objects/fill/Fill.py
@@ -1,5 +1,8 @@
 import lib
 
-class Fill:    
+
+class Fill:
+    """Classe abstrate indiquant la méthode de remplissage d'un objet"""
+
     def type(self) -> str:
         """Retourne la méthode de remplissage de l'objet"""
--- game/objects/fill/Pattern.py
+++ game/objects/fill/Pattern.py
@@ -2,25 +2,24 @@ import lib
 
 from .Fill import Fill
 
+
 class Pattern(Fill):
+    """Remplissage selon un pattern, tel un répétition d'une image"""
+
     _repeat: str
     _source: str
 
-    def __init__(self, type: str, repeat: str, sourceURL: str) -> None:
-        self._type = type
+    def __init__(self, repeat: str, sourceURL: str) -> None:
         self._repeat = repeat
         self._source = sourceURL
 
     def type(self) -> str:
-        if self._type != "Pattern":
-            raise "Le pattern attendu est ""Pattern"", or celui reçu est:" + self._type
-        else:
-            return self._type
+        return "Pattern"
 
     def repeat(self) -> str:
         """Retourne la méthode de remplissage du pattern"""
         return self._repeat
 
     def source(self) -> str:
-        """Retourne la source (URL) du pattern"""
+        """Retourne la source (URL) de l'image"""
         return self._source
