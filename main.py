92aa3fbc2d8df166ea6f2bc7c287585367724c27
lj44-ch <admin@lj44.ch>
2022-03-17 18:21:09 +0100

    ajout à sentry

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# import sentry_sdk
+# sentry_sdk.init(
+#     "https://fe63c5db7e2a4112b551a318061e751a@o1107229.ingest.sentry.io/6263977",
+
+#     # Set traces_sample_rate to 1.0 to capture 100%
+#     # of transactions for performance monitoring.
+#     # We recommend adjusting this value in production.
+#     traces_sample_rate=1.0
+# )
+
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.core import window
